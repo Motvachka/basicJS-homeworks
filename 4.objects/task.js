32a30c0982dfc7ce5230930e389284d1c35d87d3
Matvey Shapkov <matveyka1504@mail.ru>
2024-03-29 02:05:15 +0500

    выполнил task 4 objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,28 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = []; 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if ("marks" in this) {
+        this.marks.push(...marksToAdd);
+    }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function (...marksToAdd) {
+    if ("marks" in this && this.marks.length !== 0) {
+        return this.marks.reduce((a, b) => a + b, 0) / this.marks.length;
+    } else return 0
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason;
+    delete this.marks;
+    delete this.subject;
 }
